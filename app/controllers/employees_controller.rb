b8fb138a074ef22c9a21a3de5ea5f0ef02afee8b
Candace Eckels <candace.eckels@gmail.com>
2022-07-25 12:00:53 -0600

    One-third of the way through user story 2

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,7 @@ class EmployeesController < ApplicationController
     def show
         @department = Department.find(params[:id])
         @employees = Department.find(params[:id]).employees
+        @tickets
     end
     
 end
